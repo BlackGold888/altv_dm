a8ad2afa6c7fe41ee35ed0e98f7703589013c5a7
BlackGold <khamidalakkhali@gmail.com>
2023-04-14 21:12:55 +0300

    update

--- src/core/server/playerDeath/index.ts
+++ src/core/server/playerDeath/index.ts
@@ -1,5 +1,28 @@
 import * as alt from 'alt-server';
 import { FractionsInfo } from '../auth/index.js'
+// @ts-ignore
+import * as chat from "chat";
+
+const randomMessages = [
+    'Дал в рот',
+    'Трахнул в попку',
+    'Выйбал',
+    'Жёстко трахнул',
+    'Асадил',
+    'ЗАСАДИЛ',
+    'ЗАСАДИЛ без резины',
+    'ЗАСАДИЛ без вазелина',
+    'обоссал',
+    'отправил в военкамат',
+    'отправил груз 200',
+    'насрал в рот',
+    'Отдал повестку',
+    'Отдал повестку в загробный мир',
+    'Отдал 500 в ебало',
+    'отпидарасил в рот',
+    'У него сейчас бой',
+    'сделал из него АСПИДА',
+]
 
 alt.on('playerDeath', (player: alt.Player, killer: alt.Player, weapon: number) => {
     // @ts-ignore
@@ -7,5 +30,7 @@ alt.on('playerDeath', (player: alt.Player, killer: alt.Player, weapon: number) =
     player.spawn(position.x, position.y, position.z, 0);
 
     if (!killer) return;
+    const message: string = `${killer.name} ${randomMessages[Math.floor(Math.random() * randomMessages.length)]} ${player.name}`;
+    chat.broadcast(message);
     alt.emitAllClients("playerKill", { killerName: killer.name, killerGang: killer.getMeta('fraction'), victimName: player.name, victimGang: killer.getMeta('fraction'), weapon: weapon });
 })
